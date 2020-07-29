f6116001e6230ad6807824cb0102bb7d7222cd37
akilhylton <hyltonakil@gmail.com>
2020-07-28 23:59:46 -0400

    Added idle pulse effect to led

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -8,47 +8,48 @@
 #define TX 10
 #define BUTTON_PIN 3
 #define STATUS_PIN 9
-#define COUNTDOWN_NUM 59
-
+#define COUNTDOWN_NUM 60
 
 TM1637Display display(CLK, DIO);
 SoftwareSerial HC12(RX, TX);
 
 volatile int state = 0;
+int brightness = 0;   
+int fadeAmount = 5;    
 int startButton = 0;
 void countdown_timer();
-void status_indicator();
+void ready_indicator();
+void idle_indicator();
 void updateState();
+
 
 void setup() {
   display.setBrightness(0x0f);
   attachInterrupt(digitalPinToInterrupt(3), updateState, FALLING);
   pinMode(STATUS_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
-  Serial.begin(9600);
   HC12.begin(9600);
 }
 
 void loop() {
 	switch(state) {
-		case 0:
-		  Serial.println("READY! YOU CAN NOW PUSH THE BUTTON.");
-		  break;
+		case 0: // IDLE
+		 idle_indicator();
+		 break;
 
-		case 1:
-		  HC12.write('0'); // Timer beginning
-                  status_indicator();
+		case 1: // COUNTDOWN
+		  HC12.write('0'); // Timer start
+                  ready_indicator();
                   countdown_timer();
 		  break; 
 
-		case 2:
-                  Serial.println("STOP");
+		case 2: // ABORT
                   display.clear();
                   state = 0;
                   break;
 	        
-		case 3:
-		  HC12.write('1'); // Timer ending
+		case 3: // LAUNCH
+		  HC12.write('1'); // Timer done
 		  state = 0;
 		  break;
 
@@ -56,12 +57,10 @@ void loop() {
 		  break;
 	}
 
-	Serial.print("state = ");
-	Serial.println(state);
 }
 
 
-void status_indicator() {
+void ready_indicator() {
   for (int i = 0; i < 5; i++) {
     digitalWrite(STATUS_PIN, HIGH);
     delay(100);
@@ -70,11 +69,20 @@ void status_indicator() {
   }
 }
 
+void idle_indicator() {
+  analogWrite(STATUS_PIN, brightness);
+  brightness = brightness + fadeAmount;
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  delay(30);
+}
+
 void countdown_timer() {
   int i = COUNTDOWN_NUM;
   while (i >= 0) {
     i--;
-    display.showNumberDec(i);	  
+    display.showNumberDecEx(i, (0x80 >> 1), true);	  
     delay(1000);
    if (i == 0) { // If T-0 move to state 3
     state = 3;
@@ -90,7 +98,6 @@ void countdown_timer() {
 void updateState() {
  static unsigned long last_interrupt_time = 0;
  unsigned long interrupt_time = millis();
- // If interrupts come faster than 200ms, assume it's a bounce and ignore
  if (interrupt_time - last_interrupt_time > 200) {
       if (state < 2) {
 	 state++;
